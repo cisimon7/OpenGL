958ba34bdb5e6c9e7e1e603bf790e3f7affa3b42
cisimon7 <cisimon7@gmail.com>
2022-01-10 13:27:20 +0300

    Triangle changed to Square

--- main.cpp
+++ main.cpp
@@ -31,10 +31,19 @@ struct Vertex {
     float r, g, b;
 };
 
-Vertex vertices[3] = {
-        {-0.6f, -0.4f, 1.f, 0.f, 0.f},
-        {+0.6f, -0.4f, 0.f, 1.f, 0.f},
-        {+0.0f, +0.6f, 0.f, 0.f, 1.f}
+Vertex vertices[] = {
+        {-0.5f, +0.5f, 1.f, 0.f, 0.f},
+        {+0.5f, -0.5f, 0.f, 1.f, 0.f},
+        {+0.5f, +0.5f, 0.f, 0.f, 1.f},
+
+        // {+0.5f, -0.5f, 1.f, 1.f, 0.f},
+        // {-0.5f, +0.5f, 0.f, 1.f, 1.f},
+        {-0.5f, -0.5f, 0.f, 0.f, 1.f},
+};
+
+unsigned int indices[] {
+    0, 1, 2,
+    0, 1, 3
 };
 
 struct ShaderProgramSource {
@@ -67,25 +76,6 @@ static ShaderProgramSource parseShader(const std::string &filePath) {
     return {shaders[(int) ShaderType::VERTEX].str(), shaders[(int) ShaderType::FRAGMENT].str()};
 }
 
-//static const char *vertex_shader_text =
-//        "#version 110\n"
-//        "uniform mat4 MVP;\n"
-//        "attribute vec3 vCol;\n"
-//        "attribute vec2 vPos;\n"
-//        "varying vec3 color;\n"
-//        "void main()\n"
-//        "{\n"
-//        "    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
-//        "    color = vCol;\n"
-//        "}\n";
-//
-//static const char *fragment_shader_text =
-//        "#version 110\n"
-//        "varying vec3 color;\n"
-//        "void main()\n"
-//        "{\n"
-//        "    gl_FragColor = vec4(color, 1.0);\n"
-//        "}\n";
 
 static unsigned int compile_shader(unsigned int type, const char *source) {
 
@@ -124,9 +114,6 @@ static unsigned int create_shader(const std::string &vertex_text, const std::str
     vertex_shader = compile_shader(GL_VERTEX_SHADER, vText);
     fragment_shader = compile_shader(GL_FRAGMENT_SHADER, fText);
 
-//    assert(vertex_shader != 0);0
-//    assert(fragment_shader != 0);
-
     program = glCreateProgram();
     glAttachShader(program, vertex_shader);
     glAttachShader(program, fragment_shader);
@@ -141,7 +128,7 @@ static unsigned int create_shader(const std::string &vertex_text, const std::str
 
 int main() {
     GLFWwindow *window;
-    unsigned int vertex_buffer, program;
+    unsigned int vertex_buffer, program, index_buffer;
     unsigned int vertex_shader, fragment_shader;
     GLuint mvp_location, vpos_location, vcol_location;
 
@@ -180,11 +167,11 @@ int main() {
 
     glGenBuffers(1, &vertex_buffer); /* Create for me a buffer */
     glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer); /* Set buffer type */
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices,
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(Vertex), vertices,
                  GL_STATIC_DRAW); /* Creates and initializes a buffer object's data store */
 
     const auto[vertex_shader_text, fragment_shader_text] =
-        parseShader("res/shaders/Basic.shader");
+    parseShader("res/shaders/Basic.shader");
     program = create_shader(vertex_shader_text, fragment_shader_text);
 
     mvp_location = glGetUniformLocation(program, "MVP");
@@ -195,6 +182,13 @@ int main() {
     glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE, sizeof(vertices[0]), nullptr);
     glEnableVertexAttribArray(vcol_location);
     glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE, sizeof(vertices[0]), (void *) (sizeof(float) * 2));
+
+
+    glGenBuffers(1, &index_buffer); /* Create for me a buffer */
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer); /* Set buffer type */
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices,
+                 GL_STATIC_DRAW); /* Creates and initializes a buffer object's data store */
+
 
     glUseProgram(program);
 
@@ -222,7 +216,8 @@ int main() {
         mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f); /* Project in orthogonal view */
         mat4x4_mul(mvp, p, m); /* Final Matrix after rotation and projection */
         glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat *) mvp);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
         /* Swapping of buffers after each frame has been rendered */
         glfwSwapBuffers(window);
